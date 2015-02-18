51b3a1bbc3fa31fcb9f72e10463ac17a2c36403d
Chandra Carney <chandracarney@me.com>
2015-02-18 14:19:19 -0700

    First row pushing left and adding elements together. Test passing.

--- test/movement-test.js
+++ test/movement-test.js
@@ -8,3 +8,13 @@ test('It can push elements all the way to the left', function(assert) {
 
   assert.deepEqual(board.firstRow, [2, null, null, null], 'It moved!');
 });
+
+test('Push adds elements of the same number', function(assert) {
+  var board = new Board();
+  board.addNumberToColumn(2, 3)
+  board.addNumberToColumn(2, 1)
+
+  board.pushLeft();
+
+  assert.deepEqual(board.firstRow, [4, null, null, null], 'It moved!');
+})
