9b0a47fce1c5b50b7d7d37c7ea1d52e5da5683b6
Chandra Carney <chandracarney@me.com>
2015-02-18 15:33:25 -0700

    Finished pushLeft movement, starting pushRight movement.

--- test/push-left-movement-test.js
+++ test/push-left-movement-test.js
@@ -19,6 +19,18 @@ test('Push adds elements of the same number', function(assert) {
   assert.deepEqual(board.firstRow, [4, null, null, null], 'It moved and added numbers!');
 });
 
+test('Push does not recursively add all numbers', function(assert) {
+  var board2 = new Board();
+  board2.addNumberToColumn(2, 1);
+  board2.addNumberToColumn(2, 2);
+  board2.addNumberToColumn(2, 3);
+  board2.addNumberToColumn(2, 4);
+
+  board2.pushLeft();
+
+  assert.deepEqual(board2.firstRow, [4, 4, null, null], 'It moved and added sets.');
+});
+
 test('Push does not add elements of different numbers', function(assert) {
   var board2 = new Board();
   board2.addNumberToColumn(2, 1);
@@ -27,4 +39,16 @@ test('Push does not add elements of different numbers', function(assert) {
   board2.pushLeft();
 
   assert.deepEqual(board2.firstRow, [2, 4, null, null], 'It moved and did not add!');
-})
+});
+
+test('Push does not add or move when numbers are all different.', function(assert) {
+  var board2 = new Board();
+  board2.addNumberToColumn(16, 1);
+  board2.addNumberToColumn(8, 2);
+  board2.addNumberToColumn(4, 3);
+  board2.addNumberToColumn(2, 4);
+
+  board2.pushLeft();
+
+  assert.deepEqual(board2.firstRow, [16, 8, 4, 2], 'It did not move and did not add!');
+});
