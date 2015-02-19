d1ca98b0eb30fbda9b1edb7ff9917bf58ea85e09
Travis Yoder <trayoda@gmail.com>
2015-02-19 16:15:31 -0700

    wip

---
+++ test/grid-test.js
@@ -0,0 +1,24 @@
+module('Grid Test');
+
+test('It makes an array.', function(assert) {
+  var board = new Board();
+
+  assert.deepEqual(board.firstRow, [ null, null, null, null ], 'Array return.');
+});
+
+test('It can put one element.', function(assert) {
+  var board = new Board();
+
+  board.addNumberToColumn(2, 4)
+
+  assert.deepEqual(board.firstRow, [null, null, null, 2], 'It has one element.')
+});
+
+test('It can put two elements.', function(assert) {
+  var board = new Board();
+
+  board.addNumberToColumn(2, 4)
+  board.addNumberToColumn(4, 3)
+
+  assert.deepEqual(board.firstRow, [null, null, 4, 2], 'It has two elements.')
+});
